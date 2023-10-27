df73d51b4a8ceb47b45e365a8641014159f7178b
Vryph <vschmaedecke@gmail.com>
2023-10-27 20:51:52 -0300

    Aspect Ratio
    Consertei o problema da resolução que aconteceu na build

--- Assets/Scripts/cameraSettings.cs
+++ Assets/Scripts/cameraSettings.cs
@@ -7,27 +7,12 @@ public class cameraSettings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Resolution[] resolutions = Screen.resolutions;
-
-        foreach (var res in resolutions)
-        {
-            Debug.Log(res.width + "w" + res.height + "h : " + res.refreshRate);
-        }
+        Debug.Log(Screen.currentResolution);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Screen.SetResolution(640, 480, true);
-        if (Input.GetKeyDown(KeyCode.C))
-       { 
-            Screen.SetResolution(640, 480, true);
 
-            for (int i = 0; i < 5; i++)
-            {
-                Debug.Log(Screen.currentResolution);
-            }
-
-       }
     }
 }
